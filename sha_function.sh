272775aa96fcf05b56d7155a608a1c725027a3b4
Anna X <vmnet8@gmail.com>
2020-06-12 23:11:18 -0700

    add comment out

--- sha_function.sh
+++ sha_function.sh
@@ -5,10 +5,7 @@ alpine_sha (){
     alpine_repo=$1
     #docker pull -q $1 &>/dev/null
     docker pull $1 &>/dev/null
-   # docker pull -q  ${docker_image} &>/dev/null
     sha=$(docker image history $1 |awk '{print$1}' |tail -2 |head -1)
-    #alpine_sha=$(docker image history $1 |awk '{print$1}' |tail -2 |head -1)
-    #echo $alpine_sha
     echo $sha
 }
 
@@ -30,6 +27,6 @@ compare (){
 }
 
 
-alpine_sha "vmnet8/alpine:latest"
+#alpine_sha "vmnet8/alpine:latest"
 #nginx_sha vmnet8/nginx-tags:alpine-x86
 #compare alpine_sha "treehouses/alpine:3.11" nginx_sha "treehouses/nginx:latest"
