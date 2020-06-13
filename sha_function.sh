a54084b4e969af701b7435bdb1734da382733409
Anna X <vmnet8@gmail.com>
2020-06-12 22:40:43 -0700

    add quite mode

--- sha_function.sh
+++ sha_function.sh
@@ -3,7 +3,7 @@
 alpine_sha (){
     local sha
     alpine_repo=$1
-    docker pull $1
+    docker pull $1 -q &>/dev/null
     sha=$(docker image history $1 |awk '{print$1}' |tail -2 |head -1)
     #alpine_sha=$(docker image history $1 |awk '{print$1}' |tail -2 |head -1)
     #echo $alpine_sha
@@ -13,7 +13,7 @@ alpine_sha (){
 nginx_sha (){
     local sha
     nginx_repo=$1
-    docker pull $1
+    docker pull -q $1 &>/dev/null
     sha=$(docker image history $1 |awk '{print$1}' |tail -2 |head -1)
     echo $sha
 }
