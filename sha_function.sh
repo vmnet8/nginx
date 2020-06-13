7d7a77e6eb8d73fa6996f2e6ccd6bccbf932aa56
Anna X <vmnet8@gmail.com>
2020-06-12 21:32:36 -0700

    comment out set -x

--- sha_function.sh
+++ sha_function.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+#set -x
 alpine_sha (){
     local sha
     alpine_repo=$1
@@ -11,10 +11,11 @@ alpine_sha (){
 }
 
 nginx_sha (){
-    local nginx_repo=$1
+    local sha
+    nginx_repo=$1
 
-    nginx_sha=$(docker image history $1 |awk '{print$1}' |tail -2 |head -1)
-    echo $nginx_sha
+    sha=$(docker image history $1 |awk '{print$1}' |tail -2 |head -1)
+    echo $sha
 }
 
 compare (){
@@ -27,6 +28,6 @@ compare (){
 }
 
 
-alpine_sha "vmnet8/alpine:latest"
+#alpine_sha "vmnet8/alpine:latest"
 #nginx_sha vmnet8/nginx-tags:alpine-x86
 #compare alpine_sha "treehouses/alpine:3.11" nginx_sha "treehouses/nginx:latest"
